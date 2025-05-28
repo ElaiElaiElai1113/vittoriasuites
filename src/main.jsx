b7e9b86159932436201b4d1e5482c9fe95979eb8
ElaiElaiElai1113 <ensdelossantos@addu.edu.ph>
2025-05-28 18:06:56 +0800

    Added ANIMATE ON SCROLL (AOS) and initialized it

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,35 @@
+// src/main.jsx (or src/main.js)
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./index.css";
-import App from "./App";
+import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+AOS.init({
+  disable: false,
+  startEvent: "DOMContentLoaded",
+  initClassName: "aos-init",
+  animatedClassName: "aos-animate",
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  offset: 120,
+  delay: 0,
+  duration: 800,
+  easing: "ease",
+  once: true,
+  mirror: false,
+  anchorPlacement: "top-bottom",
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
